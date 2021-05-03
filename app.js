246fe55d7f7bf0a3e6724e42ed2523feba0e3d40
riffatrehman123 <riffatabdulrehman78@gmail.com>
2021-05-03 20:04:26 +0500

    project complete

---
+++ app.js
@@ -0,0 +1,41 @@
+        // #table
+// for(var i=1;i<=20 ;i++){
+//     document.write("8 * "+i+ '='+8*i+"<br>");
+// }
+
+
+
+//  #Arrays
+
+// var arr=["abdul","73","true"]
+// console.log(arr[0])
+
+// var arr1=["arisha","22"]
+// console.log(arr1[0])
+
+
+// var names=["rubab","esha","jannat"]
+// names.pop()
+// console.log(names)
+
+// var names=["rubab","esha","jannat"]
+//  names.push("emaan","aysha")
+//  console.log(names)
+
+//  var names=["rubab","esha","jannat"]
+//    names.shift()
+//    console.log(names)
+
+// var names=["rubab","esha","jannat"]
+//    names.unshift("shanzay","anosha","rahat")
+//    console.log(names)
+
+// var names=["rubab","esha","jannat"]
+//     names.splice(0,2,"romana")
+//     console.log(names)
+
+
+// var names=["rubab","esha","jannat", "farhat","hijab"]
+//  var copyNames = names.slice(1,4)
+//  console.log(names)
+//      console.log(copyNames)
